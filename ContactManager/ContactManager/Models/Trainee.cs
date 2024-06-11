efdc648b750f17ef47afef48cb15d3a6cdc08994
Caner Kaya <caner.kaya2003@outlook.com>
2024-06-11 15:53:16 +0200

    Forgot to modify constructors for child classes of modified classes in previous commit

--- ContactManager/ContactManager/Models/Trainee.cs
+++ ContactManager/ContactManager/Models/Trainee.cs
@@ -1,6 +1,8 @@
-﻿namespace ContactManager.Models;
+﻿using ContactManager.Enums;
 
-public class Trainee(bool gender, string title, string firstName, string lastName, DateTime dateOfBrith, string socialSecurityNumber, CommuncationInfo communcationInfo, Address address, bool status, string nationality, Guid employeeNumber, string department, DateTime startDate, DateTime endDate, int employment, string role, int cadreLevel, int traineeYears, int actualTraineeYear) : Employee(gender, title, firstName, lastName, dateOfBrith, socialSecurityNumber, communcationInfo, address, status, nationality, employeeNumber, department, startDate, endDate, employment, role, cadreLevel)
+namespace ContactManager.Models;
+
+public class Trainee(bool gender, string title, string firstName, string lastName, DateTime dateOfBrith, string socialSecurityNumber, CommuncationInfo communcationInfo, Address address, bool status, string nationality, Guid employeeNumber, Department department, DateTime startDate, DateTime endDate, int employment, string role, CadreLevel cadreLevel, int traineeYears, int actualTraineeYear) : Employee(gender, title, firstName, lastName, dateOfBrith, socialSecurityNumber, communcationInfo, address, status, nationality, employeeNumber, department, startDate, endDate, employment, role, cadreLevel)
 {
     public int TraineeYears { get; set; } = traineeYears;
     public int ActualTraineeYear { get; set; } = actualTraineeYear;
