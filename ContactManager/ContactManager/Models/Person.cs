6a993f91504fd4ef3a38d75d95632f3460fb4924
Caner Kaya <caner.kaya2003@outlook.com>
2024-06-11 15:39:46 +0200

    Fixed typos

--- ContactManager/ContactManager/Models/Person.cs
+++ ContactManager/ContactManager/Models/Person.cs
@@ -1,14 +1,14 @@
 ﻿namespace ContactManager.Models;
 
-public class Person(bool gender, string title, string firstName, string lastName, DateTime dateOfBrith, string socialSecurityNumber, CommuncationInfo communcationInfo, Address address, bool status, string nationality)
+public class Person(bool gender, string title, string firstName, string lastName, DateTime dateOfBirth, string socialSecurityNumber, CommuncationInfo communicationInfo, Address address, bool status, string nationality)
 {
     public bool Gender { get; set; } = gender;
     public string Title { get; set; } = title;
     public string FirstName { get; set; } = firstName;
     public string LastName { get; set; } = lastName;
-    public DateTime DateOfBrith { get; set; } = dateOfBrith;
+    public DateTime DateOfBirth { get; set; } = dateOfBirth;
     public string SocialSecurityNumber { get; set; } = socialSecurityNumber;
-    public CommuncationInfo CommuncationInfo { get; set; } = communcationInfo;
+    public CommuncationInfo CommunicationInfo { get; set; } = communicationInfo;
     public Address Address { get; set; } = address;
     public bool Status { get; set; } = status;
     public string Nationality { get; set; } = nationality;
