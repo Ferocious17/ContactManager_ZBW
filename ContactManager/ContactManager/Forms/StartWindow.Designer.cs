22f5b2a07c78706d614141d7a83690edc497c40e
Caner Kaya <caner.kaya2003@outlook.com>
2024-07-02 14:04:55 +0200

    Made start window non resizable and repositioned elements

--- ContactManager/ContactManager/Forms/StartWindow.Designer.cs
+++ ContactManager/ContactManager/Forms/StartWindow.Designer.cs
@@ -28,29 +28,21 @@
         /// </summary>
         private void InitializeComponent()
         {
-            btnUserRegistration = new Button();
             btnSearch = new Button();
             btnClientRegistration = new Button();
             txtBoxTitel = new TextBox();
+            btnUserRegistration = new Button();
+            tableLayoutPanel1 = new TableLayoutPanel();
+            tableLayoutPanel1.SuspendLayout();
             SuspendLayout();
-            // 
-            // btnUserRegistration
-            // 
-            btnUserRegistration.Location = new Point(52, 120);
-            btnUserRegistration.Margin = new Padding(1, 1, 1, 1);
-            btnUserRegistration.Name = "btnUserRegistration";
-            btnUserRegistration.Size = new Size(161, 55);
-            btnUserRegistration.TabIndex = 0;
-            btnUserRegistration.Text = "Mitarbeiter Erfassung";
-            btnUserRegistration.UseVisualStyleBackColor = true;
-            btnUserRegistration.Click += btnUserRegistration_Click;
             // 
             // btnSearch
             // 
-            btnSearch.Location = new Point(385, 121);
-            btnSearch.Margin = new Padding(1, 1, 1, 1);
+            btnSearch.Anchor = AnchorStyles.Left | AnchorStyles.Right;
+            btnSearch.Location = new Point(431, 36);
+            btnSearch.Margin = new Padding(1);
             btnSearch.Name = "btnSearch";
-            btnSearch.Size = new Size(99, 54);
+            btnSearch.Size = new Size(213, 54);
             btnSearch.TabIndex = 1;
             btnSearch.Text = "Suche";
             btnSearch.UseVisualStyleBackColor = true;
@@ -58,49 +50,81 @@
             // 
             // btnClientRegistration
             // 
-            btnClientRegistration.Location = new Point(235, 120);
-            btnClientRegistration.Margin = new Padding(1, 1, 1, 1);
+            btnClientRegistration.Anchor = AnchorStyles.Left | AnchorStyles.Right;
+            btnClientRegistration.Location = new Point(216, 35);
+            btnClientRegistration.Margin = new Padding(1);
             btnClientRegistration.Name = "btnClientRegistration";
-            btnClientRegistration.Size = new Size(124, 55);
+            btnClientRegistration.Size = new Size(213, 55);
             btnClientRegistration.TabIndex = 2;
-            btnClientRegistration.Text = "Kuden Erfassung";
+            btnClientRegistration.Text = "Kunden Erfassung";
             btnClientRegistration.UseVisualStyleBackColor = true;
             btnClientRegistration.Click += btnClientRegistration_Click;
             // 
             // txtBoxTitel
             // 
             txtBoxTitel.Font = new Font("Segoe UI", 15F);
-            txtBoxTitel.Location = new Point(58, 34);
-            txtBoxTitel.Margin = new Padding(1, 1, 1, 1);
-            txtBoxTitel.Multiline = true;
+            txtBoxTitel.Location = new Point(39, 43);
+            txtBoxTitel.Margin = new Padding(1);
             txtBoxTitel.Name = "txtBoxTitel";
-            txtBoxTitel.Size = new Size(253, 41);
-            txtBoxTitel.TabIndex = 3;
+            txtBoxTitel.Size = new Size(267, 41);
+            txtBoxTitel.TabIndex = 0;
+            txtBoxTitel.TabStop = false;
             txtBoxTitel.Text = "CONTACT MANAGER";
             txtBoxTitel.TextChanged += txtBoxTitel_TextChanged;
+            // 
+            // btnUserRegistration
+            // 
+            btnUserRegistration.Anchor = AnchorStyles.Left | AnchorStyles.Right;
+            btnUserRegistration.Location = new Point(1, 35);
+            btnUserRegistration.Margin = new Padding(1);
+            btnUserRegistration.Name = "btnUserRegistration";
+            btnUserRegistration.Size = new Size(213, 55);
+            btnUserRegistration.TabIndex = 0;
+            btnUserRegistration.Text = "Mitarbeiter Erfassung";
+            btnUserRegistration.UseVisualStyleBackColor = true;
+            btnUserRegistration.Click += btnUserRegistration_Click;
+            // 
+            // tableLayoutPanel1
+            // 
+            tableLayoutPanel1.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            tableLayoutPanel1.ColumnCount = 3;
+            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 33.3333321F));
+            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 33.3333321F));
+            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 33.3333321F));
+            tableLayoutPanel1.Controls.Add(btnUserRegistration, 0, 0);
+            tableLayoutPanel1.Controls.Add(btnClientRegistration, 1, 0);
+            tableLayoutPanel1.Controls.Add(btnSearch, 2, 0);
+            tableLayoutPanel1.Location = new Point(39, 120);
+            tableLayoutPanel1.Name = "tableLayoutPanel1";
+            tableLayoutPanel1.RowCount = 1;
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Percent, 50F));
+            tableLayoutPanel1.Size = new Size(645, 126);
+            tableLayoutPanel1.TabIndex = 4;
             // 
             // StartWindow
             // 
             AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(601, 253);
+            CausesValidation = false;
+            ClientSize = new Size(726, 322);
+            Controls.Add(tableLayoutPanel1);
             Controls.Add(txtBoxTitel);
-            Controls.Add(btnClientRegistration);
-            Controls.Add(btnSearch);
-            Controls.Add(btnUserRegistration);
-            Margin = new Padding(1, 1, 1, 1);
+            FormBorderStyle = FormBorderStyle.FixedDialog;
+            Margin = new Padding(1);
+            MaximizeBox = false;
             Name = "StartWindow";
             Text = "Contact Manager";
             Load += StartWindow_Load;
+            tableLayoutPanel1.ResumeLayout(false);
             ResumeLayout(false);
             PerformLayout();
         }
 
         #endregion
-
-        private Button btnUserRegistration;
         private Button btnSearch;
         private Button btnClientRegistration;
         private TextBox txtBoxTitel;
+        private Button btnUserRegistration;
+        private TableLayoutPanel tableLayoutPanel1;
     }
 }
